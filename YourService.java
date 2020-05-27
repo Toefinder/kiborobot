f2ac1f74df8151d6e4ab1bf6c881be06a73f51a0
Quang Anh <43576719+Toefinder@users.noreply.github.com>
2020-05-27 22:23:39 +0800

    Move to P1-1, send QR code
    1) Use the wrapper provided by Sample APK to move to P1-1
    2) Use Zxing to read QR code. Done this by adding to build.gradle(Module: app) file. 
    compile "com.google.zxing:core:3.3.0" // for QR 
    compile 'com.google.zxing:javase:3.3.0' // for QR
    3) Potential issues: 
    a) RGBLuminanceSource does not handle rotation, and it uses RGB which may be a waste (?) because our image is black and white (?)
    b) Does not handle the exception that the bitmap image is not found (possibly due to errors in orientation of the Astrobee)

--- YourService.java
+++ YourService.java
@@ -1,11 +1,22 @@
-package jp.jaxa.iss.kibo.rpc.sampleapk;
+package jp.jaxa.iss.kibo.rpc.defaultapk;
 
-import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
 
 import gov.nasa.arc.astrobee.Result;
+import gov.nasa.arc.astrobee.android.gs.MessageType;
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
-import com.google.zxing.*; // for QR code reader
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
+import static android.content.ContentValues.TAG;
+
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee
  */
@@ -13,6 +24,7 @@ import com.google.zxing.*; // for QR code reader
 public class YourService extends KiboRpcService {
     @Override
     protected void runPlan1(){
+        // write here your plan 1
         // start this run
         api.judgeSendStart();
 
@@ -21,15 +33,16 @@ public class YourService extends KiboRpcService {
         // once Astrobee came to P1-1, get a camera image
         Bitmap snapshot = api.getBitmapNavCam();
         // read the QR code in the image and get the x-axis coordinate value of P3
-        String valueX = decodeQR(snapshot);
+        String valueX = readQRImage(snapshot);
         // send the result to scoring module
         api.judgeSendDiscoveredQR(0, valueX);
 
-        api.laserControl(true);
-        moveToWrapper(11.5, -5.7, 4.5, 0, -0.7071068, 0, 0.7071068);
+//        api.laserControl(true);
+//        moveToWrapper(11.5, -5.7, 4.5, 0, -0.7071068, 0, 0.7071068);
 
         api.judgeSendFinishSimulation();
         sendData(MessageType.JSON, "data", "SUCCESS:defaultapk runPlan1");
+
     }
 
     @Override
@@ -41,7 +54,6 @@ public class YourService extends KiboRpcService {
     protected void runPlan3(){
         // write here your plan 3
     }
-
     // You can add your method
     private void moveToWrapper(double pos_x, double pos_y, double pos_z,
                                double qua_x, double qua_y, double qua_z,
@@ -50,7 +62,7 @@ public class YourService extends KiboRpcService {
         final int LOOP_MAX = 3;
         final Point point = new Point(pos_x, pos_y, pos_z);
         final Quaternion quaternion = new Quaternion((float)qua_x, (float)qua_y,
-                                                     (float)qua_z, (float)qua_w);
+                (float)qua_z, (float)qua_w);
 
         Result result = api.moveTo(point, quaternion, true);
 
@@ -61,9 +73,23 @@ public class YourService extends KiboRpcService {
         }
     }
 
-    private String decodeQR(Bitmap image) { // for QR code reading
-        return new MultiFormatReader().decode(bitmap).getText();
-    }
+    public static String readQRImage(Bitmap bMap) {
+        int[] intArray = new int[bMap.getWidth() * bMap.getHeight()];
+        //copy pixel data from the Bitmap into the 'intArray' array
+        bMap.getPixels(intArray, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
 
+        RGBLuminanceSource source = new RGBLuminanceSource(bMap.getWidth(), bMap.getHeight(), intArray);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        MultiFormatReader reader = new MultiFormatReader();// use this otherwise ChecksumException
+
+        try {
+            String contents = reader.decode(bitmap).getText();
+            return contents;
+        } catch (NotFoundException e) {
+            Log.e(TAG, "decode exception", e);
+            return null;
+        }
+    }
 }
 
